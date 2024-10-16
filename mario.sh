132725cfe5c29efa7b46f8f5a2bbeca37821aaa4
Roberto Pérez <rperez@infinitumdigital.net>
2024-10-16 13:35:19 +0200

    added mario emoji art

--- mario.sh
+++ mario.sh
@@ -2,12 +2,31 @@
 
 TIME=0.125
 
-play -n synth $TIME sine 659  
-play -n synth $TIME sine 659
+play -n synth $TIME sine 659 &> /dev/null
+play -n synth $TIME sine 659 &> /dev/null
 sleep $TIME
-play -n synth $TIME sine 659
+play -n synth $TIME sine 659 &> /dev/null
 sleep $TIME
-play -n synth $TIME sine 523
-play -n synth $TIME sine 659
+play -n synth $TIME sine 523 &> /dev/null
+play -n synth $TIME sine 659 &> /dev/null
 sleep $TIME
-play -n synth $TIME sine 784
+play -n synth $TIME sine 784 &> /dev/null
+
+echo ""
+echo "      🟥🟥🟥🟥🟥"
+echo "    🟥🟥🟥🟥🟥🟥🟥🟥🟥"
+echo "    🟫🟫🟫🟧🟫🟧🟧"
+echo "  🟫🟧🟫🟧🟧🟫🟧🟧🟧"
+echo "  🟫🟧🟫🟫🟧🟧🟫🟧🟧🟧"
+echo "  🟫🟫🟧🟧🟧🟫🟫🟫🟫"
+echo "      🟧🟧🟧🟧🟧🟧"
+echo "    🟫🟫🟥🟫🟫🟫"
+echo "  🟫🟫🟫🟥🟫🟫🟥🟫🟫🟫"
+echo "🟫🟫🟫🟥🟧🟥🟥🟥🟫🟫🟫🟫"
+echo "🟧🟧🟥🟥🟧🟥🟥🟧🟥🟫🟧🟧"
+echo "🟧🟧🟧🟥🟥🟥🟥🟥🟥🟧🟧🟧"
+echo "🟧🟧🟥🟥🟥🟥🟥🟥🟥🟥🟧🟧"
+echo "    🟥🟥🟥    🟥🟥🟥"
+echo "  🟫🟫🟫        🟫🟫🟫"
+echo "🟫🟫🟫🟫        🟫🟫🟫🟫"
+echo ""
